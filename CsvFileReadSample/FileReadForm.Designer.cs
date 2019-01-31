a1ebc71cbe88b1a2223fb9b378bf6646c3975e6a
backyarD46 <backyard46@gmail.com>
2019-01-31 16:31:09 +0900

    プロジェクト ファイルを追加します。

---
+++ CsvFileReadSample/FileReadForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CsvFileReadSample
+{
+    partial class FileReadForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gridMainData = new System.Windows.Forms.DataGridView();
+            this.panelTop.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridMainData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 360);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(426, 53);
+            this.panelBottom.TabIndex = 1;
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.buttonOpenFile);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(426, 53);
+            this.panelTop.TabIndex = 2;
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(16, 8);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(128, 40);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "ファイルを開く";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gridMainData);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(426, 307);
+            this.panel1.TabIndex = 3;
+            // 
+            // gridMainData
+            // 
+            this.gridMainData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridMainData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridMainData.Location = new System.Drawing.Point(0, 0);
+            this.gridMainData.Name = "gridMainData";
+            this.gridMainData.RowTemplate.Height = 21;
+            this.gridMainData.Size = new System.Drawing.Size(426, 307);
+            this.gridMainData.TabIndex = 4;
+            // 
+            // FileReadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 413);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panelBottom);
+            this.Font = new System.Drawing.Font("Meiryo UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FileReadForm";
+            this.Text = "CSVファイル読み込み";
+            this.panelTop.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridMainData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView gridMainData;
+    }
+}
+
---
+++ CsvFileReadSample/FileReadForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CsvFileReadSample
+{
+    public partial class FileReadForm : Form
+    {
+        public FileReadForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ファイルを開くボタン押下時処理。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            // 課題1
+            // ファイル名を自分で指定できるようにしてください。
+            // 「FleOpenDialog」で検索して、C#が持っているファイル指定用の画面を使い、ファイルを選択利用できるようにしましょう。
+
+            // 課題2
+            // Shift-JISで記述されたCSVファイルを開けるように改造してみましょう。
+
+            // usingでStreamReaderの利用を宣言してファイルを読み込む
+            using (StreamReader reader = new StreamReader("CsvFile/懇親会予算見積もり.csv", Encoding.GetEncoding("utf-8")))
+            {
+                string oneLineString = string.Empty;
+                string[] oneLineData;
+
+                // 最初の1行を読んで見出しにする
+                oneLineString = reader.ReadLine();
+                oneLineData = oneLineString.Split(',');
+                // 見出しを作る（Columnsに列名情報をAddする）
+                foreach (string columnName in oneLineData)
+                {
+                    gridMainData.Columns.Add(columnName,columnName);
+                }
+
+                // 2行目以降、最後の行までを1行ずつ読んでDataGridViewに追加する
+                while (!reader.EndOfStream)
+                {
+                    oneLineString = reader.ReadLine();
+                    gridMainData.Rows.Add(oneLineString.Split(','));
+                }
+            }
+
+        }
+    }
+}
